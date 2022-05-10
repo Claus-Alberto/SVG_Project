5644c0964c108e397fa45f40f8c720a35b834001
Claus Alberto Bienemann <91323034+Claus-Alberto@users.noreply.github.com>
2022-05-09 23:34:30 -0300

    Add files via upload

--- src/main/java/com/glauber/svg/SVGMain.java
+++ src/main/java/com/glauber/svg/SVGMain.java
@@ -15,33 +15,33 @@ import java.util.List;
 public class SVGMain {
 
    public static void main(String[] args) throws IOException {
+        byte[] bytes;
+        double step = 0.5;
         int interacoes = 0;
-        String begin = "";
         int graus = 0;
-        String stringAtual;
+        Map<String, String> regras = new HashMap<String, String>();
         Matcher matcher;
+        String begin = "";
+        String stringAtual;
         String primeiraPalavra;
         String segundaPalavra;
-        Path arquivo = Paths.get("input.txt");
-        List<String> lista = Files.readAllLines(arquivo, StandardCharsets.UTF_8);
-        Pattern caracteresPermitidos = Pattern.compile("p\\d : *(C|F|\\[|\\]\\+|-|) *-> *([C|F|\\+|\\-|\\[|\\]]+)");
         StringBuilder stringBuildavel = new StringBuilder();
-        Map<String, String> regras = new HashMap<String, String>();
+        StringBuilder linha;
+        StringBuilder build = new StringBuilder();
+        Path arquivo = Paths.get("input.txt");
+        Path arquivoFinal = Paths.get("result.html");
+        Pattern caracteresPermitidos = Pattern.compile("p\\d : *(C|F|\\[|\\]\\+|-|) *-> *([C|F|\\+|\\-|\\[|\\]]+)");
+        List<String> lista = Files.readAllLines(arquivo, StandardCharsets.UTF_8);
+        List<String> linhas = new ArrayList<>();
         List<Double> x = new ArrayList<>();
         List<Double> y = new ArrayList<>();
         List<Double> angulo = new ArrayList<>();
-        List<String> linhas = new ArrayList<>();
-        double step = 0.5;
-        StringBuilder linha;
-        Path arquivoFinal;
-        StringBuilder build = new StringBuilder();
-        byte[] bytes;
 
-        for (String line : lista) {
-            if (line != null) {
-                matcher = caracteresPermitidos.matcher(line);
-                primeiraPalavra = line.split(":")[0].strip();
-                segundaPalavra = line.split(":")[1].strip();
+        for (String linhaDaLista : lista) {
+            if (linhaDaLista != null) {
+                matcher = caracteresPermitidos.matcher(linhaDaLista);
+                primeiraPalavra = linhaDaLista.split(":")[0].strip();
+                segundaPalavra = linhaDaLista.split(":")[1].strip();
                 if ("n".equals(primeiraPalavra)) interacoes = Integer.parseInt(segundaPalavra);
                 else if (matcher.find()) regras.put(matcher.group(1), matcher.group(2));
                 else if ("Dg".equals(primeiraPalavra)) graus = Integer.parseInt(segundaPalavra);
@@ -49,25 +49,23 @@ public class SVGMain {
             }
         }
         stringBuildavel.append(begin);
-        System.out.println("n = 0: " + begin);
-        for (int i = 1; i <= interacoes; i++) {
+        for (int count = 1; count <= interacoes; count++) {
             stringAtual = stringBuildavel.toString();
             stringBuildavel.setLength(0);
             for (char letter : stringAtual.toCharArray()) {
                 if (!regras.containsKey(String.valueOf(letter))) stringBuildavel.append(String.valueOf(letter));
                 else stringBuildavel.append(regras.get(String.valueOf(letter)));
             }
-            System.out.println(stringBuildavel.toString());
         }
         x.add((double) 40);
         y.add((double) 50);
         angulo.add(Math.toRadians(-60));
         x.add(x.get(x.size() - 1) + (step * Math.cos(angulo.get(angulo.size() - 1))));
         y.add(y.get(y.size() - 1) + (step * Math.sin(angulo.get(angulo.size() - 1))));
-        for(char c : stringBuildavel.toString().toCharArray()){
-            if(c == 'C' || c == 'F'){
+        for(char charDaString : stringBuildavel.toString().toCharArray()){
+            if(charDaString == 'C' || charDaString == 'F'){
                 linha = new StringBuilder();
-                linha.append("<line x1=\"");
+                linha.append("<linhaDaLista x1=\"");
                 linha.append(x.get(x.size()-2));
                 linha.append("%\" y1=\"");
                 linha.append(y.get(y.size()-2));
@@ -82,23 +80,23 @@ public class SVGMain {
                 x.add(x.get(x.size() - 1) + (step * Math.cos(angulo.get(angulo.size() - 1))));
                 y.add(y.get(y.size() - 1) + (step * Math.sin(angulo.get(angulo.size() - 1))));
             }
-            else if(c == ']'){
+            else if(charDaString == ']'){
                 x.remove(x.size() - 1);
                 x.remove(x.size() - 1);
                 y.remove(y.size() - 1);
                 y.remove(y.size() - 1);
                 angulo.remove(angulo.size() - 1);
             }
-            else if(c == '['){
+            else if(charDaString == '['){
                 x.add(x.get(x.size() - 2));
                 x.add(x.get(x.size() - 2));
                 y.add(y.get(y.size() - 2));
                 y.add(y.get(y.size() - 2));
                 angulo.add(angulo.get(angulo.size() - 1));
             }
-            else if(c == '+' || c == '-'){
-                if(c == '+') angulo.add(angulo.get(angulo.size() - 1) - Math.toRadians(graus));
-                else if(c == '-') angulo.add(angulo.get(angulo.size() - 1) + Math.toRadians(graus));
+            else if(charDaString == '+' || charDaString == '-'){
+                if(charDaString == '+') angulo.add(angulo.get(angulo.size() - 1) - Math.toRadians(graus));
+                else if(charDaString == '-') angulo.add(angulo.get(angulo.size() - 1) + Math.toRadians(graus));
                 angulo.remove(angulo.size() - 2);
                 x.remove(x.size()-1);
                 y.remove(y.size()-1);
@@ -106,9 +104,8 @@ public class SVGMain {
                 y.add(y.get(y.size() - 1) + (step * Math.sin(angulo.get(angulo.size() - 1))));
             }
         }
-        arquivoFinal = Paths.get("result.html");
-        build.append("<html><body style=\"background-color:white;\"><div style=\"position: fixed; top: 0; z-index: 1000;\"></div><svg id=\"svgZoom\" viewBox=\"0 0 3000 3000\" preserveAspectRatio=\"xMidYMid meet\" style=\"stroke:black;stroke-width:2\">");
-        for(String line : linhas) build.append(line);
+        build.append("<html><body><div style=\"position: fixed; top: 0; z-index: 1000;\"></div><svg id=\"svgZoom\" viewBox=\"0 0 3000 3000\" preserveAspectRatio=\"xMidYMid meet\" style=\"stroke:black;stroke-width:2\">");
+        for(String linhaDaLista : linhas) build.append(linhaDaLista);
         build.append("</svg></body><script>const slider = document.getElementById(\"zoomRange\");const svgZoom = document.getElementById(\"svgZoom\");const zoom = document.getElementById(\"zoom\");slider.oninput = function() {zoom.innerText = `${this.value}%`;svgZoom.style.transform = `scale(${this.value / 100})`;}</script></html>");
         bytes = build.toString().getBytes();
         Files.write(arquivoFinal, bytes);
